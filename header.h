56fc31a545597d646fff7b2bae935a5608365583
Michael <michaeldoyon1@gmail.com>
2021-11-20 15:43:55 -0500

    Second Commit

---
+++ header.h
@@ -0,0 +1,13 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Michael Doyon
+ */
+
+
+#ifndef MAIN_CPP_HEADER_H
+#define MAIN_CPP_HEADER_H
+
+#include <iostream>
+#include <math.h>
+
+#endif
--- main.cpp
+++ main.cpp
@@ -3,81 +3,20 @@
  *  Copyright 2021 Michael Doyon
  */
 
-#include "std_lib_facilities.h"
+#include "header.h"
 
-double a, b, c;
-bool do_check;
-
+using namespace std;
 
 
-double found(double x)
+void quad_solver(double a, double b, double c)
 {
-    if (!do_check)
-        return x;
+    double res = 0;
+    double res2 = 0;
 
-    else
-    {
-        const double epsilon = 1e-7;
-        double e = a*x*x + b*x + c;
+    res = (-b + sqrt((pow(b, 2) - 4*a*c))) / (2*a);
+    res2 = (-b - sqrt((pow(b, 2) - 4*a*c))) / (2*a);
 
-        if (e == 0)
-            return x;
-
-        if (0 < e && epsilon < e)
-            cout << "poor root; off by " << e << endl;
-
-        else if (e < -epsilon)
-            cout << "poor root; off by " << e << endl;
-
-        return x;
-    }
-}
-
-
-
-void quad_solver()
-{
-    while (cin >> a >> b >> c)
-    {
-        if (a == 0)
-        {
-            if (b == 0)
-                cout << "no root (x isn't used)\n";
-
-            else
-                cout << "x = " << found(-c/b) << endl;
-        }
-
-        else if (b == 0)
-        {
-            double ca = -c/a;
-
-            if (ca == 0)
-                cout << "x = 0\n";
-
-            else if (ca < 0)
-                cout << "no real roots\n";
-
-            else
-                cout << "two real roots: x = " << found(sqrt(ca)) << found(-sqrt(ca)) << endl;
-        }
-
-        else
-        {
-            double delta = b*b - 4*a*c;
-
-            if (delta == 0)
-                cout << "one real root: " << found(-b/(2*a)) << endl;
-
-            else if (delta < 0)
-                cout << "no real roots\n";
-
-            else
-                cout << "two real roots: " << found((-b + sqrt(delta)) / (2*a)) << " and " << found((-b - sqrt(delta)) / (2*a)) << endl;
-        }
-
-        cout << "try again: ";
-    }
+    cout << "x = " << res << " and " << res2 << endl;
 }
 
 
@@ -85,39 +24,17 @@ void quad_solver()
 
 int main()
 {
-    try
-    {
-        cout << "Should your solutions be checked?\n";
-        char check = 0;
-        cin >> check;
+    double a = 0, b = 0, c = 0;
+    
+    cout << "Enter a then b then c using the form ax^2 + bx + c = 0." << endl;
+    cin >> a >> b >> c;
 
-        if (check == 'Y' || check == 'y')
-            do_check = true;
+    if ((pow(b, 2) - 4*a*c) < 0)
+        cout << "No real roots." << endl;
 
-        else if (check == 'N' || check == 'n')
-            do_check = false;
+    else
+        quad_solver(a, b, c);
 
-        else
-            error("Answer not recognised (try 'y' or 'n')");
-
-        cout << "Enter coefficients for quadratic equation a*x^2 + b*x + c = 0\n";
-
-        quad_solver();
-
-        return 0;
-    }
-
-    catch (exception& e)
-    {
-        cerr << "Error: " << e.what() << endl;
-        keep_window_open();
-        return 1;
-    }
-
-    catch (...)
-    {
-        cerr << "Unknown exception!\n";
-        return 2;
-    }
+    return 0;
     
 }
